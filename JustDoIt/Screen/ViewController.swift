b70dcc784380bc532d91523eee154db3f9ec19c7
Pavel Krivtsov <pavel.krivtsov@yandex.ru>
2022-04-24 01:48:29 +0300

    Add creation and deletion of tasks from the table with Core Data

--- JustDoIt/Screen/ViewController.swift
+++ JustDoIt/Screen/ViewController.swift
@@ -6,10 +6,11 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UITableViewController {
     
-    var tasks = [String]()
+    var tasks = [Task]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +24,19 @@ class ViewController: UITableViewController {
         tableView.register(TaskCell.self, forCellReuseIdentifier: TaskCell.reuseId)
     }
     
-    @objc func addTask() {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let context = getContext()
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        do {
+            tasks = try context.fetch(fetchRequest )
+        } catch {
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+    
+    @objc private func addTask() {
         let addTaskController = UIAlertController(title: "Enter Task",
                                                   message: nil,
                                                   preferredStyle: .alert)
@@ -31,8 +44,8 @@ class ViewController: UITableViewController {
         let cansel = UIAlertAction(title: "Cancel", style: .cancel)
         let add = UIAlertAction(title: "Add", style: .default) { action in
             let textf = addTaskController.textFields?.first
-            if let newTask = textf?.text {
-                self.tasks.append(newTask)
+            if let task = textf?.text {
+                self.saveTask(withTitle: task)
                 self.tableView.reloadData()
             }
         }
@@ -40,11 +53,35 @@ class ViewController: UITableViewController {
         addTaskController.addAction(add)
         present(addTaskController, animated: true)
     }
+
+    private func saveTask(withTitle title: String) {
+        let context = getContext()
+        guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        let taskObject = Task(entity: entity, insertInto: context)
+        taskObject.title = title
+        do {
+            try context.save()
+            tasks.append(taskObject)
+        } catch {
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+    
+    private func getContext() -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+}
+
+extension ViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: TaskCell.reuseId, for: indexPath) as? TaskCell {
+            let task = tasks[indexPath.row]
             var content = cell.defaultContentConfiguration()
-            content.text = tasks[indexPath.row]
+            content.text = task.title
             cell.contentConfiguration = content
             return cell
         }
@@ -55,5 +92,24 @@ class ViewController: UITableViewController {
         tasks.count
     }
     
-}
+    override func tableView(_ tableView: UITableView,
+                            trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _, _, _ in
+            let context = self.getContext()
+            let task = self.tasks[indexPath.row]
+            context.delete(task)
 
+            do {
+                try context.save()
+                self.tasks = self.tasks.filter { $0 != task }
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+        let swipe = UISwipeActionsConfiguration(actions: [deleteAction])
+        return swipe
+    }
+    
+}
