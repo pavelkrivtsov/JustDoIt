e611612ffd742bd1308ebf0270c644ea2e96944b
Pavel Krivtsov <pavel.krivtsov@yandex.ru>
2022-04-23 17:31:04 +0300

    Add alert controller for adding a task

--- JustDoIt/ViewController.swift
+++ JustDoIt/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "JustDoIt"
@@ -20,16 +20,28 @@ class ViewController: UITableViewController {
     }
     
     @objc func addTask() {
-        
+        let addTaskController = UIAlertController(title: "Enter Task",
+                                                  message: nil,
+                                                  preferredStyle: .alert)
+        addTaskController.addTextField { textField in
+            
+        }
+        let cansel = UIAlertAction(title: "Cansel", style: .cancel)
+        let add = UIAlertAction(title: "Add", style: .default) { action in
+            
+        }
+        addTaskController.addAction(cansel)
+        addTaskController.addAction(add)
+        present(addTaskController, animated: true)
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         UITableViewCell()
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         5
     }
-
+    
 }
 
